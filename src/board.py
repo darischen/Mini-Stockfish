e2e05837453e5e000c7cedf5efd52a240e7026f0
Daris Chen <daris.chen@gmail.com>
2025-03-29 19:52:16 -0700

    Render Pieces

--- src/board.py
+++ src/board.py
@@ -1,10 +1,13 @@
 from const import *
 from square import Square
+from piece import *
 
 class Board:
     def __init__(self):
         self.squares = [[0, 0, 0, 0, 0, 0, 0, 0] for col in range(COLS)]
         self._create()
+        self._add_pieces('white')
+        self._add_pieces('black')
 
     
     def _create(self):
@@ -13,4 +16,24 @@ class Board:
                 self.squares[row][col] = Square(row, col)
     
     def _add_pieces(self, color):
-        pass
+        row_pawn, row_other = (6, 7) if color == 'white' else (1, 0)
+        
+        # Create all pawns
+        for col in range(COLS):
+            self.squares[row_pawn][col] = Square(row_pawn, col, Pawn(color))
+            
+        # Create all Knights
+        self.squares[row_other][1] = Square(row_other, 1, Knight(color))
+        self.squares[row_other][6] = Square(row_other, 6, Knight(color))
+        
+        # Create all Bishops
+        self.squares[row_other][2] = Square(row_other, 2, Bishop(color))
+        self.squares[row_other][5] = Square(row_other, 5, Bishop(color))
+        
+        # Create all Rooks
+        self.squares[row_other][0] = Square(row_other, 0, Rook(color))
+        self.squares[row_other][7] = Square(row_other, 7, Rook(color))
+        
+        # Create Queen and King
+        self.squares[row_other][3] = Square(row_other, 3, Queen(color))
+        self.squares[row_other][4] = Square(row_other, 4, King(color))
