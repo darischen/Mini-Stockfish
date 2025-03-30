c9079d8f153336692fc75482ecff5fb57568720e
Daris Chen <daris.chen@gmail.com>
2025-03-29 18:59:56 -0700

    Created Console Board

---
+++ src/const.py
@@ -0,0 +1,8 @@
+#Screen dim
+WIDTH = 800
+HEIGHT = 800
+
+#Board dim
+ROWS = 8
+COLS = 8
+SQSIZE = WIDTH // COLS
---
+++ src/piece.py
@@ -0,0 +1,5 @@
+
+
+class Piece:
+    def __init__(self, name, color, value, texture, teture_rect=None):
+        pass
