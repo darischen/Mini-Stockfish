e5b258f91676c87fc929511c57e933555f3cc124
Daris Chen <daris.chen@gmail.com>
2025-05-17 23:56:21 -0700

    Depth 3 test

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ class Main:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Chess AI")
         self.game = Game()
-        self.ai = ChessAI(depth=5, use_dnn=True)
+        self.ai = ChessAI(depth=3, use_dnn=True)
 
     def mainloop(self):
         game = self.game
